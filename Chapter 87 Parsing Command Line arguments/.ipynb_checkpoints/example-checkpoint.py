0beabd8c706df187d90458bab918bcad750bac6f
songchaodevip <songcmail@gmail.com>
2018-03-05 22:40:42 +0800

    删除一些错误缓存的文件

--- Chapter 87 Parsing Command Line arguments/.ipynb_checkpoints/example-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='Simple example')
-parser.add_argument('name', help='Who to greet', default='World')
-parser.add_argument('--bar_this')
-parser.add_argument('--bar_that')
-parser.add_argument('--foo_this')
-parser.add_argument('--foo_that')
-args = parser.parse_args()
--- Chapter 87 Parsing Command Line arguments/.ipynb_checkpoints/user-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import getpass
-import sys
-words = sys.argv[1:]
-sentence = " ".join(words)
-print("[%s] %s" % (getpass.getuser(), sentence))
